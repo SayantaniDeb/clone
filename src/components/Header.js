e651c7b8df4f98dac336a32d3b5398aaba79da90
SayantaniDeb <74983536+SayantaniDeb@users.noreply.github.com>
2022-01-30 02:47:00 +0530

    Active user details created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { connect } from "react-redux";
 
 const Header=(props)=>{
     return (
@@ -56,9 +57,14 @@ const Header=(props)=>{
 
             <User>
               <a>
+                {props.user && props.user.photoURL ?(
+                  <img src={props.user.photoURL} alt=""/>
+                ):(
                 <img src="/images/user.svg" alt="" />
-                <span>Me</span>
+                )}
+                <span>Me
                 <img src="/images/down-icon.svg" alt="" />
+                </span>
               </a>
 
               <SignOut>
@@ -68,6 +74,7 @@ const Header=(props)=>{
 
             <Work>
               <a>
+                
                 <img src="/images/nav-work.svg" alt="" />
                 <span>
                   Work
@@ -250,4 +257,12 @@ const Work = styled(User)`
   border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
 
-export default Header;
+const mapStateToProps =(state) => {
+  return{
+    user: state.userState.user,
+  };
+};
+const mapDispatchToProps =(dispatch) => ({});
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Header);
